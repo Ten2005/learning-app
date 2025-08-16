df46fa6a62f0b0fa400bf0d3634b32aa60c13db7
Ten2005 <stuzeukni0901@icloud.com>
2025-08-17 06:54:04 +0900

    edit title

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -3,20 +3,24 @@
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { useDashboardStore } from "@/store/dashboard";
-import { TelescopeIcon } from "lucide-react";
+import { CheckIcon, PencilIcon, TelescopeIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import CreatePageButton from "@/components/dashboard/createPageButton";
 import PageButtons from "@/components/dashboard/pageButton";
+import { Input } from "@/components/ui/input";
+import { useSidebarStore } from "@/store/sidebar";
+import { updateFileAction } from "./actions";
 
 export default function Dashboard() {
-  const { currentFile, setCurrentFile } = useDashboardStore();
+  const { currentFile, setCurrentFile, isEditingTitle } = useDashboardStore();
+  const { currentFolder } = useSidebarStore();
 
   return (
     <div className="flex flex-col gap-2 w-full h-full">
       <div className="flex flex-col gap-2 justify-between border-b p-2">
         <div className="flex flex-row justify-between">
-          <Label className="text-primary">{currentFile?.title}</Label>
-          <PageButtons />
+          {currentFile && (isEditingTitle ? <EditTitle /> : <ShowTitle />)}
+          {currentFolder && <PageButtons />}
         </div>
         <div className="flex flex-row items-end justify-between">
           <Button variant="outline" size="icon" className="size-8">
@@ -37,5 +41,63 @@ export default function Dashboard() {
         resize-none border-none focus:border-none focus-visible:ring-0"
       />
     </div>
+  );
+}
+
+function EditTitle() {
+  const { currentFile, setCurrentFile, setIsEditingTitle } =
+    useDashboardStore();
+
+  const handleUpdateTitle = () => {
+    setIsEditingTitle(false);
+    if (currentFile) {
+      updateFileAction(
+        currentFile.id,
+        currentFile.title || "",
+        currentFile.content || "",
+      );
+    }
+  };
+
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <Button variant="ghost" size="icon" onClick={handleUpdateTitle}>
+        <CheckIcon className="w-4 h-4 text-muted-foreground" />
+      </Button>
+      <Input
+        type="text"
+        value={currentFile?.title || ""}
+        onChange={(e) => {
+          if (currentFile) {
+            setCurrentFile({ ...currentFile, title: e.target.value });
+          }
+        }}
+      />
+    </div>
+  );
+}
+
+function ShowTitle() {
+  const { setIsEditingTitle } = useDashboardStore();
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => setIsEditingTitle(true)}
+      >
+        <PencilIcon className="w-4 h-4 text-muted-foreground" />
+      </Button>
+      <TitleLabel />
+    </div>
+  );
+}
+
+function TitleLabel() {
+  const { currentFile } = useDashboardStore();
+  return currentFile?.title ? (
+    <Label className="text-primary">{currentFile?.title}</Label>
+  ) : (
+    <Label className="text-muted-foreground">None</Label>
   );
 }
--- store/dashboard.ts
+++ store/dashboard.ts
@@ -4,9 +4,15 @@ import { UsedFile } from "@/types/sidebar/file";
 interface DashboardState {
   currentFile: UsedFile | undefined;
   setCurrentFile: (currentFile: UsedFile | undefined) => void;
+
+  isEditingTitle: boolean;
+  setIsEditingTitle: (isEditingTitle: boolean) => void;
 }
 
 export const useDashboardStore = create<DashboardState>((set) => ({
   currentFile: undefined,
   setCurrentFile: (currentFile) => set({ currentFile }),
+
+  isEditingTitle: false,
+  setIsEditingTitle: (isEditingTitle) => set({ isEditingTitle }),
 }));

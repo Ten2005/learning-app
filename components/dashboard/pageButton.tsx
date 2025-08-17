2a1cd443abb230193372bfc206cf6c15da6981c6
Ten2005 <stuzeukni0901@icloud.com>
2025-08-17 17:25:38 +0900

    page change

--- components/dashboard/pageButton.tsx
+++ components/dashboard/pageButton.tsx
@@ -2,14 +2,37 @@
 
 import { Button } from "../ui/button";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { useDashboardStore } from "@/store/dashboard";
+import { useSidebarStore } from "@/store/sidebar";
 
 export default function PageButtons() {
+  const { currentFile, setCurrentFile } = useDashboardStore();
+  const { currentFiles } = useSidebarStore();
+  const handlePageChange = (page: number) => {
+    if (!currentFile) return;
+    const newCurrentFile = currentFiles.find((file) => file.page === page);
+    if (!newCurrentFile) return;
+    setCurrentFile(newCurrentFile);
+  };
+  if (!currentFile) return;
   return (
     <div className="flex flex-row gap-2">
-      <Button size="icon" variant="ghost" className="size-8">
+      <Button
+        size="icon"
+        variant="ghost"
+        className="size-8"
+        onClick={() => handlePageChange(currentFile.page - 1)}
+        disabled={currentFile.page === 0}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Button size="icon" variant="ghost" className="size-8">
+      <Button
+        size="icon"
+        variant="ghost"
+        className="size-8"
+        onClick={() => handlePageChange(currentFile.page + 1)}
+        disabled={currentFile.page === currentFiles.length - 1}
+      >
         <ChevronRightIcon />
       </Button>
     </div>

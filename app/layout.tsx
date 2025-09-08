63157332b8df65769f2613730757ba03fb32f80a
Ten Suzuki <122023835+Ten2005@users.noreply.github.com>
2025-09-08 09:10:31 +0900

    chore: refine auth loading and update lockfile

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
 import { PRODUCT_NAME, PRODUCT_DESCRIPTION } from "@/constants/index";
 
 const inter = Inter({
@@ -29,6 +30,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>

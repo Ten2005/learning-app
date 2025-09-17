13d16a16614f35d396ae52a028e30f5167e33774
Ten2005 <stuzeukni0901@icloud.com>
2025-09-17 17:19:24 +0900

    pwa?2

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,14 +12,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/logo.svg",
-        sizes: "192x192",
-        type: "image/svg+xml",
+        src: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
       },
       {
-        src: "/logo.svg",
-        sizes: "512x512",
-        type: "image/svg+xml",
+        src: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
       },
     ],
   };
